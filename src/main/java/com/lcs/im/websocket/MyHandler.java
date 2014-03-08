683df0d387176992340398f0d6de821f7708f353
Johnson <a598420668@gmail.com>
2014-03-08 17:56:02 +0800

    加入websocket配置

---
+++ src/main/java/com/lcs/im/websocket/MyHandler.java
@@ -0,0 +1,14 @@
+package com.lcs.im.websocket;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+public class MyHandler extends TextWebSocketHandler {
+
+	@Override
+	public void handleTextMessage(WebSocketSession session, TextMessage message) {
+		System.out.println(session);
+		System.out.println(message);
+	}
+}
