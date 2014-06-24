87d606d34e43de64aab025cb16e6d4ae36f23db0
Johnson <a598420668@gmail.com>
2014-06-24 18:01:21 +0800

    day by day

--- server/server.js
+++ server/server.js
@@ -1,24 +1,68 @@
-var io = require("socket.io");
-var http = require('http');
+
 var app = require("express")();
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
+var sockets = {};
 
-var server = http.createServer(function(request,respon){
-	console.log(33);
-	respon.write("33");
-	respon.end();
+app.get("/",function(req,res){	
+	var file = __dirname +req._parsedUrl.pathname;
+	res.sendfile(file);
+	
+	console.log("---------------------------------------------------------------------------------------------------------");
+	console.log(file);
 });
 
-app.get("/*",function(req,res){
-	console.log("---------------------------------------------------------------------------------------------------------");
-	console.log(req.route);
-	console.log(req);
+app.get("/static/*",function(req,res){	
 	var file = __dirname +req._parsedUrl.pathname;
-	console.log(file);
 	res.sendfile(file);
 });
 
-app.listen(9090);
+io.on("connection",function(socket){
+	console.log("someone comes");
+	
+	sockets[socket.id] = {
+		socket:socket,
+		name:'name'
+	};
 
-//server.listen(9090);
+	var res = function(name,cb){
+		socket.on(name,function(data){
+			data = cb.call(this,data,function(data){
+				data && socket.emit(name,data);
+			});
+			data && socket.emit(name,data);
+		});
+	};
+
+	socket.on("disconnect",function(){
+		delete sockets[socket.id];
+		console.log("someone's gone");
+	}).on("message",function(data,s,e){
+		console.log(data,s,e);
+	}).on("atWho",function(){
+
+	}).on("who",function(){
+
+	}).on("name",function(name){
+
+	});
+
+	res("ha",function(data,cb){
+		console.log(data);
+		cb.call(this,"shit")
+		return "haha";
+	})
+})
+
+server.listen(9090,function(){
+	console.log("ha",server);
+});
+
+
+
+
+
+
+
 
